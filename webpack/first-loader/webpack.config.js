027cc8be8e3a8e4cc6db8a3c98c89cf3b1a7a444
awell <ranzw41@gmail.com>
2024-07-17 16:48:24 +0800

    feat: first plugin

--- webpack/first-loader/webpack.config.js
+++ webpack/first-loader/webpack.config.js
@@ -1,4 +1,31 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { Compilation } = require('webpack');
+
+
+class MyWebpackPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('MyWebpackPlugin', (compilation) => {
+      compilation.hooks.processAssets.tapPromise({
+        name: 'MyWebpackPlugin',
+        stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL,
+      }, async (compilation) => {
+        for (const name in compilation) {
+          if (name.endsWith('.js')) {
+            const content = compilation[name].source();
+            const withoutComments = content.replace(/\/\*[\s\S]*?\*\//g, '')
+            compilation[name] = {
+              source: () => withoutComments,
+              size: () => withoutComments.length
+            }
+          }
+        }
+      });
+    });
+  }
+}
 
 module.exports = {
   mode: 'none',
@@ -6,7 +33,27 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    // publicPath: 'dist/'
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Plugin sample',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1'
+      },
+      template: './src/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'share.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public", to: "" },
+      ],
+    }),
+    new MyWebpackPlugin()
+  ],
   module: {
     rules: [
       {
