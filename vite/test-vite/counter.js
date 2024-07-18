9763e73e2cdfe5c932cb11f19629cbf9caf57f9f
awell <ranzw41@gmail.com>
2024-07-18 15:21:33 +0800

    feat: env config

--- vite/test-vite/counter.js
+++ vite/test-vite/counter.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import lodashES from 'lodash-es'
 
-//
-
 console.log('lodash', _)
 export const count = 0
+
+console.log(import.meta.env)
--- vite/test-vite/vite.config.js
+++ vite/test-vite/vite.config.js
@@ -1,7 +1,28 @@
+import { defineConfig, loadEnv } from 'vite'
+import viteBaseConfig from './vite.base.config'
+import viteDevConfig from './vite.dev.config'
+import viteProdConfig from './vite.prod.config'
 
-/** @type {import('vite').UserConfig} */
-export default {
-  optimizeDeps: {
-    // exclude: ['lodash-es'], // 不进行依赖预构建
+const envResolver = {
+  build: () => {
+    console.log('build')
+    return { ...viteBaseConfig, ...viteProdConfig }
+  },
+  serve: () => {
+    console.log('serve')
+    return Object.assign({}, viteBaseConfig, viteDevConfig)
   }
 }
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log('env', env.APP_KEY)
+  return envResolver[command]()
+})
+
+// /** @type {import('vite').UserConfig} */
+// export default {
+//   optimizeDeps: {
+//     exclude: ['lodash-es']  // 不进行依赖预构建
+//   }
+// }
